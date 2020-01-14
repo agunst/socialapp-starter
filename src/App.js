ce1b765317245dd284966f141da2cfb23f21eb81
Anne Gunst <aegunst7@gmail.com>
2020-01-14 16:23:08 -0500

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
   render() {
     return (
