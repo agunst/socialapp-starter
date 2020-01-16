ed7783bf63703501d47117e0e19cff7e5aaefabb
Thomas <thmasky@amazon.com>
2020-01-16 15:10:15 -0500

    (1)Created RegisterNewUser page (2)Added route to /Register in App.js (3)Added link to Register in LoginForm.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import RegisterNewUser from "./pages/RegisterNewUser";
 import NotFound from "./pages/NotFound";
 import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends React.Component {
@@ -10,20 +11,25 @@ class App extends React.Component {
     return (
       <Switch>
         <Route
-            exact
-            path="/"
-            component={Home}
-          />
-          <Route
-            exact
-            path="/profile/:username"
-            component={Profile}
-          />
-          <Route
-            exact
-            path="*"
-            component={NotFound}
-          />
+          exact
+          path="/"
+          component={Home}
+        />
+        <Route
+          exact
+          path="/profile/:username"
+          component={Profile}
+        />
+        <Route
+          exact
+          path="/Register"
+          component={RegisterNewUser}
+        />
+        <Route
+          exact
+          path="*"
+          component={NotFound}
+        />
       </Switch>
     );
   }
---
+++ src/pages/RegisterNewUser.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class RegisterNewUser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            submitted: false,
+            formData: {
+                userName: "",
+                displayName: "",
+                password: ""
+            }
+        }
+    }
+    handleChange = (event) => {
+        let formData = this.state.formData;
+        formData[event.target.name] = event.target.value;
+        this.setState({ formData });
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            submitted: true
+        })
+    }
+    // Following code copied from react-demo-app. 
+    // Saving in case we need it later.
+    // resetForm = (event) => {
+    //     this.setState({
+    //         submitted: false,
+    //         formData: {
+    //             userName: "",
+    //             displayName: "",
+    //             password: ""
+    //         }
+    //     })
+    // }
+
+    render() {
+        //show the thank you message if the form has been submitted
+        if (this.state.submitted) {
+            return (
+                <div>
+                    Thank you, {this.state.formData.userName}!
+                    <br />
+                    Your account has been created - you are now ready to log in!
+                    <button><Link to="/">Return to Log In Page</Link></button>
+                </div>
+            )
+        }
+        return (
+            <div className="formBox">
+                <form onSubmit={this.handleSubmit}>
+                    <div id="userName">
+                        <label>User Name: </label>
+                        <input onChange={this.handleChange} type="text" name="userName" value={this.state.formData.userName} required />
+                    </div>
+                    <div id="displayName">
+                        <label>Display Name: </label>
+                        <input onChange={this.handleChange} type="text" name="displayName" value={this.state.formData.displayName} required />
+                    </div>
+                    <div id="password">
+                        <label>Password: </label>
+                        <input onChange={this.handleChange} type="text" name="password" value={this.state.formData.password} required />
+                    </div>
+                    <button>Submit Form</button> <br />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default RegisterNewUser;
